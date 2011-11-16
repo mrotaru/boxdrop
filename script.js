4cc1d98518a102321d2c0355eb6f8f109eb2ec81
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-11-16 10:25:06 +0000

    add (broken) JavaScript file

---
+++ script.js
@@ -0,0 +1,19 @@
+// rudimentary function to morph from one colour to another
+function changeElementColour( element, R2, G2, B2, current_step, total_steps ) {
+    
+    color1 = window.getComputedStyle( element ).backgroundColor;
+    alert( color1 );
+
+//    R1 = element.style.backgroundColor;
+//    G1 = element.style.backgroundColor;
+//    B1 = element.style.backgroundColor;
+    
+    rd = R2 - R1;
+    gd = G2 - G1;
+    bd = B2 - B1;
+
+    R2 += rd / total_steps * current_step;
+    G2 += gd / total_steps * current_step;
+    B2 += bd / total_steps * current_step;
+
+}
