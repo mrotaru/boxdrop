664e84e13d8f5a7f749e53ebc9c70608d3f0620c
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-11-18 21:58:06 +0000

    add a small navigation bar, pages for preferences and help

--- script.js
+++ script.js
@@ -6,22 +6,14 @@ function onClickRegister() {
     window.location="register.html";
 }
 
-// rudimentary function to morph from one colour to another
-function changeElementColour( element, R2, G2, B2, current_step, total_steps ) {
-    
-    color1 = window.getComputedStyle( element ).backgroundColor;
-    alert( color1 );
+function onClickSearch() {
+    alert( "Searching for: " + document.getElementById('search').value );
+}
 
-//    R1 = element.style.backgroundColor;
-//    G1 = element.style.backgroundColor;
-//    B1 = element.style.backgroundColor;
-    
-    rd = R2 - R1;
-    gd = G2 - G1;
-    bd = B2 - B1;
+function onClickSearchTextbox() {
+        selectAll("search");
+}
 
-    R2 += rd / total_steps * current_step;
-    G2 += gd / total_steps * current_step;
-    B2 += bd / total_steps * current_step;
-
+function selectAll( id ) {
+    document.getElementById( id ).select();
 }
