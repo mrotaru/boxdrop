3f77084bca0c9bc3038ddde856dc66c12a9f3773
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-04 22:41:35 +0000

    moved js and css to their respective folders

--- includes/head.php
+++ includes/head.php
@@ -1,5 +1,5 @@
 <head>
     <title>Simple Website</title>
-    <link rel="stylesheet" type="text/css" href="style.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheets/global.css" />
     <script language="JavaScript" src="script.js"></script>
 </head>
