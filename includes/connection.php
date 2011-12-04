8e33d81c431a65ee60947189139096c914d65c1f
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-04 23:56:35 +0000

    use constants.php

--- includes/connection.php
+++ includes/connection.php
@@ -1,14 +1,15 @@
 <?php
+require( "constants.php" );
 
 // create database connection
-$connection = mysql_connect( "localhost", "root", "" );
+$connection = mysql_connect( DB_SERVER, DB_USER, DB_PASS );
 if( !$connection )
 {
     die( "Cannot connect to database: " . mysql_error());
 }
 
 // select a database
-$db_select = mysql_select_db( "test", $connection );
+$db_select = mysql_select_db( DB_NAME, $connection );
 if( !$db_select )
 {
     die( "Cannot select database: " . mysql_error());
