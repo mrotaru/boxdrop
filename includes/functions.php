d22f64166df071ebd7a31af2cd101262e4e27bc5
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-06 15:32:21 +0000

    new functions: filled_out, valid_email, mysql_prep

--- includes/functions.php
+++ includes/functions.php
@@ -34,4 +34,54 @@ function check_query( $result_set )
     }
 }
 
+// test if all form variables have values assigned to them
+function filled_out( $form_vars )
+{
+    foreach( $form_vars as $key => $value )
+    {
+        if( !isset( $key ) || ( $value == ''))
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+// sanitize $string, to make it SQL-safe
+// ~from: Lynda.com - Essential PHP with MySQL, 13-06
+function mysql_prep( $string ) {
+    $magic_quotes_active = get_magic_quotes_gpc();
+    $new_enough_php = function_exists( "mysql_real_escape_string" ); // i.e. PHP >= v4.3.0
+
+    if( $new_enough_php ) // PHP v4.3.0 or higher
+    {
+        // undo any magic quote effects so mysql_real_escape_string can do the work
+        if( $magic_quotes_active ) 
+        { 
+            $string = stripslashes( $string ); 
+        }
+        $string = mysql_real_escape_string( $string );
+    } 
+    else // before PHP v4.3.0
+    {
+        // if magic quotes aren't already on then add slashes manually
+        if( !$magic_quotes_active ) 
+        { 
+            $string = addslashes( $string ); 
+        }
+        // if magic quotes are active, then the slashes already exist
+    }
+    return $string;
+}
+
+// from: PHP and MySQL Web Development 4th Edition, p.619
+function valid_email( $email )
+{
+    if( ereg('^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+.[a-zA-Z0-9\-\.]+$', $email ))
+    {
+        return true;
+    }
+    return false;
+}
+
 ?>
