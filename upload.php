78868fc45a4c635e953e226b6c22460514a49703
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-17 00:00:55 +0000

    show errors when upload isn't successful

--- upload.php
+++ upload.php
@@ -1,12 +1,79 @@
 <?php require_once( "includes/session.php" ); ?>
 
+<?php require_once( "includes/connection.php" ); ?>
+<?php require_once( "includes/functions.php" ); ?>
+<?php require_once( "includes/box_drop_functions.php" ); ?>
+
+<?php
+
+$upload_errors = array(
+    // http://www.php.net/manual/en/features.file-upload.errors.php
+    UPLOAD_ERR_OK 			=> "No errors.",
+    UPLOAD_ERR_INI_SIZE  	=> "Larger than upload_max_filesize.",
+    UPLOAD_ERR_FORM_SIZE 	=> "Larger than form MAX_FILE_SIZE.",
+    UPLOAD_ERR_PARTIAL 		=> "Partial upload.",
+    UPLOAD_ERR_NO_FILE 		=> "No file.",
+    UPLOAD_ERR_NO_TMP_DIR   => "No temporary directory.",
+    UPLOAD_ERR_CANT_WRITE   => "Can't write to disk.",
+    UPLOAD_ERR_EXTENSION 	=> "File upload stopped by extension."
+);
+
+if(isset($_POST['submit'])) 
+{
+
+    // errors ?
+    if( $_FILES[ "form_file" ][ "error" ] != 0 )
+    {
+		$error = $_FILES[ "form_file" ][ "error" ];
+		$message = "ERROR: " . $upload_errors[ $error ];
+    }
+    else
+    {
+
+        ensure_default_folder();
+
+//        show_files_superglobal( "form_file" );
+//        show_post_superglobal();
+
+        // get info from the _FILES superglobal
+        $file_name = $_FILES[ "form_file" ][ "name" ];
+        $file_type = $_FILES[ "form_file" ][ "type" ];
+        $file_size = $_FILES[ "form_file" ][ "size" ];
+        $file_description = $_POST[ "form_description" ];
+
+        // get the raw data
+        $form_file = $_FILES[ "form_file" ][ "tmp_name" ];
+        $file_data = addslashes( 
+                        fread( 
+                            fopen( $form_file, "r"), filesize( $form_file )
+                            )
+                         );
+
+        // upload to database
+        $user_id = $_SESSION[ 'user_id' ];
+        $result = mysql_query("
+            INSERT INTO user_${user_id}_folder_root
+            ( filename, filetype, filesize, data, description ) 
+            ". " VALUES (
+                '$file_name',
+                '$file_type',
+                '$file_size',
+                '$file_data',
+                '$file_description'
+            )"
+            ); 
+        check_query( $result );
+        $message = "upload successful";
+    }
+
+}
+?>
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html>
 
-<?php require_once( "includes/connection.php" ); ?>
-<?php require_once( "includes/functions.php" ); ?>
 <?php include( "includes/head.php" ); ?>
 
 <body>
@@ -15,7 +82,9 @@
 
         <div id="content-no-margin">
 
-            <form method="post" action="process_upload.php" enctype="multipart/form-data"> 
+            <?php if(!empty($message)) { echo "<p>{$message}</p>"; } ?>
+
+            <form method="post" action="upload.php" enctype="multipart/form-data"> 
                  <div id="form-container">
                     <fieldset>
                         <input type="hidden" name="MAX_FILE_SIZE" value="1000000"/> 
