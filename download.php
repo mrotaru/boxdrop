7f9e7e4fde667bbbca8f39a494d292c803b43bac
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-17 18:49:08 +0000

    fix corrupted downloads

--- download.php
+++ download.php
@@ -1,12 +1,40 @@
 <?php require_once( "includes/session.php" ); ?>
+<?php require_once( "includes/connection.php" ); ?>
+<?php require_once( "includes/functions.php" ); ?>
+<?php
+if( isset( $_SESSION[ 'user_id' ] ))
+{
+    $user_id = $_SESSION[ 'user_id' ];
+    $file_id = $_GET[ 'id' ];
+
+    $query = "
+        SELECT filename, data, filetype, filesize
+        FROM user_${user_id}_folder_root
+        WHERE id=${file_id}
+        "; 
+    $result = mysql_query( $query ); 
+    check_query( $result );
+
+    $result_array = mysql_fetch_array( $result );
+
+    $data = $result_array[ "data" ]; 
+    $name = $result_array[ "filename" ];
+    $type = $result_array[ "filetype" ]; 
+    $size = $result_array[ "filesize" ]; 
+
+    Header( "Content-disposition: attachment; filename={$name}" );
+    Header( "Content-length: $size" );
+    Header( "Content-type: $type" );
+    echo( $data );
+    exit;
+}
+?>
 
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 
 <html>
 
-<?php require_once( "includes/connection.php" ); ?>
-<?php require_once( "includes/functions.php" ); ?>
 <?php include( "includes/head.php" ); ?>
 
 <body>
@@ -14,31 +42,6 @@
         <?php include( "includes/header.php" ); ?>
 
         <div id="content-no-margin">
-
-        <?php
-
-        if( isset( $_SESSION[ 'user_id' ] ))
-        {
-            $user_id = $_SESSION[ 'user_id' ];
-            $file_id = $_GET[ 'id' ];
-
-            $query = "
-                SELECT filename, data, filetype 
-                FROM user_${user_id}_folder_root
-                WHERE id=${file_id}
-                "; 
-            $result = mysql_query( $query ); 
-            check_query( $result );
-
-            $data = mysql_result( $result, 0, "data" ); 
-            $type = mysql_result( $result, 0, "filetype" ); 
-            $file_name = mysql_result( $result, 0, "filename" ); 
-            
-            Header( "Content-type: $type" );
-            Header( "Content-disposition: attachment; filename='{$file_name}'" );
-        }
-        ?>
-
         </div>
 
         <?php include( "includes/footer.php" ); ?>
