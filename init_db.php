db7fb5ac3ba0718fef982d6624d8e2db10647d01
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-19 08:43:39 +0000

    fix: multiple 'root' folders
    This was due to:
    - when adding a new folder, not checking if a folder with that name exists
    - when initializing the db, not dropping the 'folders' table first

--- init_db.php
+++ init_db.php
@@ -11,11 +11,18 @@
 
 require_once( "includes/connection.php" );
 require_once( "includes/functions.php" );
+require_once( "includes/box_drop_functions.php" );
 
 // delete 'users' table
 //------------------------------------------------------------------------------
 $res = mysql_query( "DROP TABLE IF EXISTS users" );
 check_query( $res );
+
+// delete 'folders' table for user 1
+//------------------------------------------------------------------------------
+$res = mysql_query( "DROP TABLE IF EXISTS user_1_folders" );
+check_query( $res );
+
 
 // create 'users' table
 //------------------------------------------------------------------------------
@@ -36,5 +43,7 @@ $res = mysql_query( "
     ( 'derp','derp@gmail.com','85136c79cbf9fe36bb9d05d0639c70c265c18d37' );
     ");
 check_query( $res );
+
+init_user( 1 );
 
 ?>
