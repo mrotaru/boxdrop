0213cf61bed729cf71dd553f39c9c63c468bb37b
Mihai Rotaru <mihai_rotaru@lavabit.com>
2011-12-19 00:21:19 +0000

    new: rename folders
    Strange: SQL produces errors when names of a table which is to be
    renamed are surrounded with quotes. Singe, double, doesn't matter
    - SQL will complain. So, I left them unqoted.

--- rename.php
+++ rename.php
@@ -12,25 +12,45 @@ if( isset( $_SESSION[ 'user_id' ] ))
         $user_id = $_SESSION[ 'user_id' ];
         if( isset( $_POST[ 'submit'] ))
         {
-            if(isset( $_POST[ 'folder_id' ] ) && isset( $_POST[ 'file_id' ])) 
-            {
-                if( isset( $_POST[ 'new_name' ] ))
-                {
-                    $folder_id = $_POST[ 'folder_id' ];
-                    $file_id   = $_POST[ 'file_id' ];
-                    $new_name  = $_POST[ 'new_name' ];
-                    $folder_name = get_folder_name_by_id( $folder_id );
+            $folder_id = $_POST[ 'folder_id' ];
+            $folder_name = get_folder_name_by_id( $folder_id );
+            $new_name  = $_POST[ 'new_name' ];
 
-                    $result = mysql_query( "
-                        UPDATE user_${user_id}_folder_${folder_name}
-                        SET filename = '$new_name' 
-                        WHERE id = '$file_id'
-                        " );
-                    check_query( $result );
-                    
-                    redirect_to( "show_folder.php?folder_id=${folder_id}" );
-                }
+            // renaming a file ?
+            if(isset( $folder_id ) && isset( $file_id ) && isset( $new_name )) 
+            {
+                $file_id   = $_POST[ 'file_id' ];
+                $result = mysql_query( "
+                    UPDATE user_${user_id}_folder_${folder_name}
+                    SET filename = '$new_name' 
+                    WHERE id = '$file_id'
+                    " );
+                check_query( $result );
+                
+                redirect_to( "show_folder.php?folder_id=${folder_id}" );
             }
+            // renaming a folder
+            else if( isset( $folder_id ) && !isset( $file_id ) && isset( $new_name) )
+            {
+                $result = mysql_query( "
+                    UPDATE user_${user_id}_folders
+                    SET name = '$new_name' 
+                    WHERE id = '$folder_id'
+                    " );
+                check_query( $result );
+
+                $result = mysql_query( "
+                    RENAME TABLE 
+                    user_${user_id}_folder_${folder_name} 
+                    TO 
+                    user_${user_id}_folder_${new_name}
+                    " );
+                check_query( $result );
+                
+                redirect_to( "show_folder.php?folder_id=1" );
+
+            }
+
         }
     }
 }
@@ -51,14 +71,22 @@ if( isset( $_SESSION[ 'user_id' ] ))
             <fieldset>
                 <?php
 
-                if( isset( $_GET[ 'folder_id' ] ))
+                if( isset( $_GET[ 'folder_id' ] ) && !isset( $_GET[ 'file_id' ] ))
+                {
                     $folder_id = $_GET[ 'folder_id' ];
+                    $folder_name = get_folder_name_by_id( $folder_id );
+                    $message = "New name for folder {$folder_name}:";
+                }
                 
-                if( isset( $_GET[ 'file_id' ] ))
+                if( isset( $_GET[ 'folder_id' ] ) && isset( $_GET[ 'file_id' ] ))
+                {
+                    $folder_id = $_GET[ 'folder_id' ];
                     $file_id = $_GET[ 'file_id' ];
+                    $file_name = get_filename_by_id( $folder_id, $file_id );
+                    $message = "New name for file {$file_name}:";
+                }
 
-                $filename = get_filename_by_id( $folder_id, $file_id );
-                echo( "<legend>New name for file '{$filename}'</legend>" );
+                echo( "<legend>{$message}</legend>" );
                 ?>
 
                 <label for="new_name"></label>
